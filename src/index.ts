dc33e1e21db5acf60f394d18d6079588b3a9dd18
Akindejoye <tydruhill@gmail.com>
2022-08-18 23:22:19 +0100

    User Object Composition

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { User } from "./models/User";
+import { User } from './models/User';
 
-const user = new User({name: 'my-name', age: 20});
-console.log(user.set({name: 'Francis', age: 40}));
+const user = new User({ name: 'Another Name', age: 12 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+
+
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,22 @@
-interface UserProps {
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
+
+ export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
-type Callback = () => {} //type alias for a callback function
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
   
-  constructor(private data: UserProps){}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback) {}
 }
